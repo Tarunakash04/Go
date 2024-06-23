23bbf1aea964f540591b217e1568281f7894d516
Tarun Akash <124859424+Tarunakash04@users.noreply.github.com>
2024-06-23 10:50:19 +0530

    Create basicgoroutine2.go

---
+++ basicgoroutine2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func f(n int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i <= n; i++ {
+		fmt.Println(i)
+		amt := time.Duration(rand.Intn(500))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func main() {
+	var n int
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	fmt.Println("Enter an integer : ")
+	fmt.Scanln(&n)
+	go f(n, &wg)
+
+	wg.Wait()
+}
